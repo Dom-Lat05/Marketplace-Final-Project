d2833279322f19088d5d0cd2b45bb38625020ade
Dominic LaTorraca <75808067+Dom-Lat05@users.noreply.github.com>
2026-04-06 10:43:10 -0400

    Create marketplace.h

---
+++ src/marketplace.h
@@ -0,0 +1,28 @@
+#ifndef MARKETPLACE_H
+#define MARKETPLACE_H
+
+#include <string>
+#include <vector>
+#include "listing.h"
+#include "user.h"
+
+class Marketplace {
+private:
+    std::vector<User> users;
+    std::vector<Listing> all_Listings;
+
+public:
+    void addUser(const User& user);
+    void addListing(const Listing& listing);
+    void displayAllListings() const;
+    void displayMyListings(const std::string& username) const;
+
+    std::vector<Listing>& getAllListings();
+    const std::vector<Listing>& getAllListings() const;
+
+    void searchListingsByName(const std::string& name) const;
+    void filterListingsByCategory(const std::string& category) const;
+    void removeListing(const std::string& listingName);
+};
+
+#endif
