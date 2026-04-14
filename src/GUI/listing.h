39878a615402d9fe75d05a2ec2f854771d725091
Dominic LaTorraca <75808067+Dom-Lat05@users.noreply.github.com>
2026-04-14 13:08:57 -0400

    Create listing.h

---
+++ src/GUI/listing.h
@@ -0,0 +1,32 @@
+#ifndef LISTING_H
+#define LISTING_H
+
+#include <QString>
+
+class Listing
+{
+public:
+    Listing();
+    Listing(const QString& title,
+            const QString& category,
+            double price,
+            const QString& description,
+            const QString& seller);
+
+    QString getTitle() const;
+    QString getCategory() const;
+    double getPrice() const;
+    QString getDescription() const;
+    QString getSeller() const;
+
+    QString toDisplayString() const;
+
+private:
+    QString m_title;
+    QString m_category;
+    double m_price;
+    QString m_description;
+    QString m_seller;
+};
+
+#endif // LISTING_H
