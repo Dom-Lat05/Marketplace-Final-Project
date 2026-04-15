da418c89f67d2d7311b75b963218a01dd8b1302f
Dominic LaTorraca <75808067+Dom-Lat05@users.noreply.github.com>
2026-04-14 23:11:58 -0400

    Refactor Listing class constructor and methods

--- src/GUI/listing.h
+++ src/GUI/listing.h
@@ -6,26 +6,24 @@
 class Listing
 {
 public:
-    Listing();
-    Listing(const QString& title,
-            const QString& category,
-            double price,
-            const QString& description,
-            const QString& seller);
+    Listing() {}
+    Listing(const QString& title, const QString& category, double price, const QString& seller)
+        : m_title(title), m_category(category), m_price(price), m_seller(seller) {}
 
-    QString getTitle() const;
-    QString getCategory() const;
-    double getPrice() const;
-    QString getDescription() const;
-    QString getSeller() const;
+    QString toDisplayString() const
+    {
+        return m_title + " | " + m_category + " | $" + QString::number(m_price) + " | " + m_seller;
+    }
 
-    QString toDisplayString() const;
+    QString getTitle() const { return m_title; }
+    QString getCategory() const { return m_category; }
+    double getPrice() const { return m_price; }
+    QString getSeller() const { return m_seller; }
 
 private:
     QString m_title;
     QString m_category;
     double m_price;
-    QString m_description;
     QString m_seller;
 };
 
