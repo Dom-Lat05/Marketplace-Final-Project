fbb1c51604beff130970ee0e96308a6eb6dec1a3
Dominic LaTorraca <75808067+Dom-Lat05@users.noreply.github.com>
2026-04-14 22:39:58 -0400

    Modify LoginDialog to accept DatabaseManager

--- src/GUI/logindialog.h
+++ src/GUI/logindialog.h
@@ -3,6 +3,7 @@
 
 #include <QDialog>
 #include "user.h"
+#include "databasemanager.h"
 
 namespace Ui {
 class LoginDialog;
@@ -13,17 +14,19 @@ class LoginDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit LoginDialog(QWidget *parent = nullptr);
+    explicit LoginDialog(DatabaseManager *db, QWidget *parent = nullptr);
     ~LoginDialog();
 
     User getUser() const;
 
 private slots:
     void on_btnLogin_clicked();
+    void on_btnCreateAccount_clicked();
 
 private:
     Ui::LoginDialog *ui;
     User m_user;
+    DatabaseManager *m_db;
 };
 
 #endif // LOGINDIALOG_H
